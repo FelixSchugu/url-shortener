340a08fd46897aea3379b5e7e47bef6831f56d5c
FelixSchugu <76438976+FelixSchugu@users.noreply.github.com>
2021-05-03 11:14:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const dns = require("dns");
 
 mongoose
   .connect(
-    "mongodb+srv://elFelix:Jujuy1030@clusterchuru.v8yea.mongodb.net/ClusterChuru?retryWrites=true&w=majority",
+    "insert db direction ",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
